a1f066de6046be9992a73653a7fa0aa476bf4749
TreVorLMAK <bimalbhandari563@gmail.com>
2025-01-11 21:51:11 +0545

    Navbar and herosection

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <>
+  <Home/>
+  </>
+
+  );
+};
+export default App;
